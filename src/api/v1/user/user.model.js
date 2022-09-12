7b6c464b8dd6f253c1f62bfbdb435fe4d1d81b63
Lê Thái Sơn <lethaison04011999@gmail.com>
2022-09-12 22:54:12 +0700

    deploy user-delete

--- src/api/v1/user/user.model.js
+++ src/api/v1/user/user.model.js
@@ -1,57 +1,59 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
+const mongooseDelete = require('mongoose-delete');
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-      maxLength: 25,
-    },
-    email: {
-      type: String,
-      trim: true,
-      required: true,
-      unique: true,
-    },
-    phone: {
-      type: String,
-      trim: true,
-      required: true,
-      maxLength: 10,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    status: {
-      type: String,
-      default: 'active',
-    },
-    verified_date: {
-      type: Date,
-      default: null,
-    },
-    deleted_at: {
-      type: Date,
-      default: null,
-    },
-    created_by: {
-      type: mongoose.Types.ObjectId,
-      ref: 'user',
-    },
-    update_by: {
-      type: mongoose.Types.ObjectId,
-      ref: 'user',
-    },
-  },
-  {
-    timestamps: true,
-  }
+	{
+		name: {
+			type: String,
+			trim: true,
+			required: true,
+			maxLength: 25,
+		},
+		email: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true,
+		},
+		phone: {
+			type: String,
+			trim: true,
+			required: true,
+			maxLength: 10,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		status: {
+			type: String,
+			default: 'active',
+		},
+		verified_date: {
+			type: Date,
+			default: null,
+		},
+		deleted_at: {
+			type: Date,
+			default: null,
+		},
+		created_by: {
+			type: mongoose.Types.ObjectId,
+			ref: 'user',
+		},
+		update_by: {
+			type: mongoose.Types.ObjectId,
+			ref: 'user',
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
 userSchema.plugin(mongoosePaginate);
+userSchema.plugin(mongooseDelete);
 
 const User = mongoose.model('User', userSchema);
 
--- src/api/v1/user/user.service.js
+++ src/api/v1/user/user.service.js
@@ -16,34 +16,32 @@ const getUser = async (id_user) => {
 };
 
 const usersList = async (search, filters = {}, options = {}) => {
-  filters.status === 'all'
-    ? (filters.status = [...STATUS_OPTIONS])
-    : (filters.status = filters.status.split(','));
+	filters.status === 'all' ? (filters.status = [...STATUS_OPTIONS]) : (filters.status = filters.status.split(','));
 
-  const limit = options.limit;
-  const offset = options.page * options.limit;
+	const limit = options.limit;
+	const offset = options.page * options.limit;
 
-  const condition = {
-    status: { $in: [...filters.status] },
-    $and: [
-      {
-        $or: [{ name: { $regex: search } }, { email: { $regex: search } }],
-      },
-      { deleted_at: null },
-    ],
-  };
+	const condition = {
+		status: { $in: [...filters.status] },
+		$and: [
+			{
+				$or: [{ name: { $regex: search } }, { email: { $regex: search } }],
+			},
+			{ deleted_at: null },
+		],
+	};
 
-  const data = await User.paginate(condition, { offset, limit });
+	const data = await User.paginate(condition, { offset, limit });
 
-  const usersPaginate = {
-    total: data.totalDocs,
-    page: data.page,
-    limit: data.limit,
-    status: filters.status,
-    users: data.docs,
-  };
+	const usersPaginate = {
+		total: data.totalDocs,
+		page: data.page,
+		limit: data.limit,
+		status: filters.status,
+		users: data.docs,
+	};
 
-  return usersPaginate;
+	return usersPaginate;
 };
 
 const checkEmailExisted = async (email) => {
